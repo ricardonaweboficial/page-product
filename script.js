b08629f7fae6e24794568435fc173bab14b1316a
RicardoWorf <steve4137@gmail.com>
2020-05-03 15:17:47 -0300

    Create function view products index, total value and dropdown

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
 const cart = document.getElementById("cart");
 const productOrders = document.getElementById('product-orders');
 const nameProduct = document.getElementById('name-product').innerText;
+const valueProduct = document.getElementById('value').innerText.substr(2,2);
 const totalView = document.getElementById("total");
+const dropdown = document.getElementsByClassName('dropdown');
 
 var countProducts = 0;
-var totalOrders = 0;
+var totalOrders = 0.00;
 
-
-function comprar() {
-
-	countProducts += 1;
+if(productOrders.innerText == '') {
+	dropdown[0].children[0].innerHTML = 'Empty';
+}
 	
-	productOrders.innerHTML += `<p>${nameProduct}</p>`		
+function comprar() {
+	countProducts += 1;
+	totalOrders += Number(valueProduct);
 
-
+	if(countProducts > 0) {
+		dropdown[0].children[0].innerHTML = 'Your cart lines:';	
+	}
 	cart.innerHTML = `<p>Carrinho: <span>${countProducts}</span></p>`;
+	productOrders.innerHTML += `<p>${countProducts}. ${nameProduct}</p>`
+	totalView.innerText = `Total: R$${totalOrders}`;
 }
 
+cart.addEventListener('click', () => {
+	dropdown[0].classList.toggle('toggle');
+});
 
 
 
